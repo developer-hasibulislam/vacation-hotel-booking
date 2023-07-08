2e5e0e6031ef7678d3a5c1d8c17032f7f4fdd0c6
developer-hasibulislam <hasib143sl@gmail.com>
2023-07-08 19:58:01 +0600

    add a loader to identify weather saved or not

--- client/pages/vendor-dashboard/add-hotel/components/AttributesTabContent.jsx
+++ client/pages/vendor-dashboard/add-hotel/components/AttributesTabContent.jsx
@@ -117,7 +117,13 @@ const AttributesTabContent = () => {
           className="button h-50 px-24 -dark-1 bg-blue-1 text-white"
           onClick={handleSaveChanges}
         >
-          Save Changes <div className="icon-arrow-top-right ml-15" />
+          {isLoading ? (
+            "Loading..."
+          ) : (
+            <>
+              Save Changes <div className="icon-arrow-top-right ml-15" />
+            </>
+          )}
         </button>
       </div>
     </div>
